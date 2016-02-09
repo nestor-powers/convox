b8e202317978cfd611231f7c34e2468255f8b4fd
arunthampi <arun.thampi@gmail.com>
2016-02-09 12:25:57 -0800

    Complete functionality for convox apps

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 module.exports = function(robot) {
-  robot.respond(/convox apps/, function(msg) {
+  robot.respond(/convox apps/, function(msg, done) {
+    msg.reply("Fetching your apps from Convox");
+
+    robot.http(process.env.CONVOX_GRID_URL + "/apps").
+          header('Version', '20150911185301').
+          auth('convox', process.env.CONVOX_GRID_PASSWORD).
+          get()(function(err, resp, body) {
+      if(resp.statusCode == 200) {
+        apps = JSON.parse(body)
+        for(var i in apps) {
+          msg.send("* " + apps[i].name + ": " + apps[i].status);
+        }
+      } else {
+        msg.reply("Oops, looks like there was an error")
+      }
+      done();
+    });
   });
 }
